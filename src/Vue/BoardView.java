ae51f8d21735d1d3d8e9b7c3b7c6a1d0c280d2c4
TheGenyus <lucas.lalida@gmail.com>
2023-11-08 16:31:09 +0100

    Change how the images are drawn

--- src/Vue/BoardView.java
+++ src/Vue/BoardView.java
@@ -1,14 +1,19 @@
 package Vue;
 
 import Controller.BoardController;
-import Model.Board;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+
+
 
 public class BoardView extends JPanel {
     private final BoardController itsBoard;
     private final Images itsImages;
+    private final int cellWidth = getWidth() / 8;
+    private final int cellHeight = getHeight() / 8;
+
 
     public BoardView(BoardController board)
     {
@@ -16,17 +21,17 @@ public class BoardView extends JPanel {
         itsImages = new Images();
     }
 
+    public void drawImage(BufferedImage image, int x, int y, Graphics g)
+    {
+        g.drawImage(image, x, y, cellWidth, cellHeight, null);
+    }
+
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
 
-        int numRows = 8;
-        int numCols = 8;
-        int cellWidth = getWidth() / numCols;
-        int cellHeight = getHeight() / numRows;
-
-        for (int row = 0; row < numRows; row++) {
-            for (int col = 0; col < numCols; col++) {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
                 if ((row + col) % 2 == 0) {
                     g.setColor(Color.LIGHT_GRAY);
                 } else {
@@ -41,19 +46,19 @@ public class BoardView extends JPanel {
                     {
                         if(itsBoard.getItsBoard()[row][col] == 1)
                         {
-                            g.drawImage(itsImages.getPionBImage(), x, y, cellWidth, cellHeight, null);
+                            drawImage(itsImages.getPionBImage(), x, y, g);
                         }
                         else if(itsBoard.getItsBoard()[row][col] == 2)
                         {
-                            g.drawImage(itsImages.getTourBImage(), x, y, cellWidth, cellHeight, null);
+                            drawImage(itsImages.getTourBImage(), x, y, g);
                         }
                         else if(itsBoard.getItsBoard()[row][col] == 3)
                         {
-                            g.drawImage(itsImages.getChevalBImage(), x, y, cellWidth, cellHeight, null);
+                            drawImage(itsImages.getChevalBImage(), x, y, g);
                         }
                         else if(itsBoard.getItsBoard()[row][col] == 4)
                         {
-                            g.drawImage(itsImages.getFouBImage(), x, y, cellWidth, cellHeight, null);
+                            drawImage(itsImages.getFouBImage(), x, y, g);
                         }
                         else if(itsBoard.getItsBoard()[row][col] == 5)
                         {
